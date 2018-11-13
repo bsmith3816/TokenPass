b8c7adf10d20ea3a9de35460434e908d11b4ba37
BT_1N3_30 <bsmith3816@bths.edu>
2018-11-13 10:17:47 -0500

    11.13

--- src/Runner.java
+++ src/Runner.java
@@ -1,20 +1,20 @@
 public class Runner {
 
-    public void main (String[] args){
+    public static void main (String[] args){
         int[] test = {0,1,9,6,8,3,11,12};
         BubbleSwap(test);
         System.out.println(test);
     }
 
-    public void swap(int[] arr, int pos1, int pos2){
+    public static void swap(int[] arr, int pos1, int pos2){
         if(arr.length-1 > pos1 && arr.length-1 > pos2){
             int x = arr[pos2];
             arr[pos2] = arr[pos1];
             arr[pos1] = x;
         }
     }
-    
-    public void BubbleSwap(int[] arr){
+
+    public static void BubbleSwap(int[] arr){
         boolean done = false;
         int m = 1;
         int x = -1;
